fe30731e1cf49a5b1788f14017c2d9de3bdf0795
capachosebas1 <84109547+capachosebas1@users.noreply.github.com>
2023-05-10 17:40:56 -0500

    1

--- autenticacion/usuario/models.py
+++ autenticacion/usuario/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Usuario(models.Model):
+    correo = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+    
+
+
+    def __str__(self):
+        return '{}'.format(self.numHistoriaClinica)
 # Create your models here.
