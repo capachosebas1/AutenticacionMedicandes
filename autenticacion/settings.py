7bc7b2760367dcb06f4125bcdbafe36a141e89a8
capachosebas1 <84109547+capachosebas1@users.noreply.github.com>
2023-05-13 08:33:25 -0500

    .

--- autenticacion/settings.py
+++ autenticacion/settings.py
@@ -143,7 +143,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/login/auth0" 
 LOGIN_REDIRECT_URL = "/" 
-LOGOUT_REDIRECT_URL = "https://medicandes.us.auth0.com/v2/logout?returnTo=http%3A%2F%2Fip_publica_instancia:8080" 
+LOGOUT_REDIRECT_URL = "https://medicandes.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F35.238.109.217:8080" 
 
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
 SOCIAL_AUTH_AUTH0_DOMAIN = 'medicandes.us.auth0.com' 
