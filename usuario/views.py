16a6bc5791e6808d0814085d600ddc1b4079a053
capachosebas1 <84109547+capachosebas1@users.noreply.github.com>
2023-05-13 00:24:32 -0500

    int

--- usuario/views.py
+++ usuario/views.py
@@ -35,8 +35,7 @@ def get_usuarios(request):
 @csrf_exempt
 def get_usuario_by_correo(request, pk):
     if request.method == 'GET':
-        correo = pk  # Obtén el valor del parámetro 'pk' como cadena de texto
-        usuario_dto = ul.get_usuario(correo)  # Obtén el usuario según el correo
+        usuario_dto = ul.get_usuario(pk)
         
         if usuario_dto is not None:
             role = getRole(usuario_dto)  # Obtén el role del usuario
